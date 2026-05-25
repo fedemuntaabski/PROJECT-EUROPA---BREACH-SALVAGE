9be10dea88b1b95439a0f39b2831d74c7bae9925
fedemuntaabski <fmuntaaabski@uade.edu.ar>
2026-05-25 11:31:13 -0300

    musica mainmenu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,4 +1,18 @@
 extends Control
 
+@onready var ambient_audio = $Ambient
+
+func _ready():
+	randomize()
+	play_random_creaks()
+
+func play_random_creaks():
+	while true:
+		var wait_time = randf_range(8.0, 20.0)
+		await get_tree().create_timer(wait_time).timeout
+		
+		ambient_audio.pitch_scale = randf_range(0.9, 1.1)
+		ambient_audio.play()
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
